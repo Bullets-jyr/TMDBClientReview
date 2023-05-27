bfdc6f00b3d8f51a57f5cd303b05abe097cd6469
Bullets-jyr <jyr@bullets.co.kr>
2023-05-27 14:47:27 +0900

    141. Create The Presentation Layer : Part 5

--- app/src/main/java/kr/co/bullets/tmdbclientreview/presentation/movie/MovieActivity.kt
+++ app/src/main/java/kr/co/bullets/tmdbclientreview/presentation/movie/MovieActivity.kt
@@ -3,6 +3,9 @@ package kr.co.bullets.tmdbclientreview.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -47,13 +50,45 @@ class MovieActivity : AppCompatActivity() {
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
             Log.i("MyTag", it.toString())
-            if(it!=null){
+            if(it != null) {
                 adapter.setList(it)
                 adapter.notifyDataSetChanged()
                 binding.movieProgressBar.visibility = View.GONE
-            }else{
+            } else {
                 binding.movieProgressBar.visibility = View.GONE
                 Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.update, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun updateMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this, Observer {
+            if(it != null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            } else {
+                binding.movieProgressBar.visibility = View.GONE
             }
         })
     }
